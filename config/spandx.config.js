5c43a1871acc2ae71670f99434c0b468337f3766
Martin Marosi <marvusm.mmi@gmail.com>
2019-03-29 14:39:25 +0100

    Migrated ui deployment to cloud.

---
+++ config/spandx.config.js
@@ -0,0 +1,8 @@
+/*global module, process*/
+
+module.exports = {
+  routes: {
+    '/hcm/approval': { host: `http://localhost:8002` },
+    '/apps/approval': { host: `http://localhost:8002` }
+  }
+};
