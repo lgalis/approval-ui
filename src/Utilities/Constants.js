472dbd6483273abde10ef6a8c505aca662f3430f
Martin Marosi <marvusm.mmi@gmail.com>
2019-04-01 11:08:40 +0200

    Migrated to new approval API client.

--- src/Utilities/Constants.js
+++ src/Utilities/Constants.js
@@ -1,4 +1,3 @@
-//export const APPROVAL_API_BASE = `${process.env.BASE_PATH}/approval/v1`;
-export const APPROVAL_API_BASE = `${process.env.BASE_PATH}/approval/v0.1`;
+export const APPROVAL_API_BASE = `${process.env.BASE_PATH}/approval/v1.0`;
 export const RBAC_API_BASE = `${process.env.BASE_PATH}/rbac/v1`;
 
