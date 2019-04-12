17da2f4d03ea85a7a0f1e915bbe89d5adeb4c8fc
Laura Galis <lgalis@redhat.com>
2019-04-12 06:15:55 -0400

    The 'Always approve' workflow should be hidden

--- src/smart-components/workflow/workflow-table-helpers.js
+++ src/smart-components/workflow/workflow-table-helpers.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 import ExpandableContent from './expandable-content';
 
-export const createInitialRows = data => data.reduce((acc, { id, name, description, group_refs }, key) => ([
-  ...acc, {
-    id,
-    isOpen: false,
-    cells: [ name, description, group_refs.length ]
-  }, {
-    parent: key * 2,
-    cells: [{ title: <ExpandableContent description={ description } groups={ group_refs } /> }]
-  }
-]), []);
+export const createInitialRows = data =>
+  data.filter(item => item.name !== 'Always approve').reduce((acc, { id, name, description, group_refs }, key) => ([
+    ...acc, {
+      id,
+      isOpen: false,
+      cells: [ name, description, group_refs.length ]
+    }, {
+      parent: key * 2,
+      cells: [{ title: <ExpandableContent description={ description } groups={ group_refs } /> }]
+    }
+  ]), []);
 
