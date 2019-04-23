67334ee1fa168a43e3c4fe5a436f7f74af50666e
Laura Galis <lgalis@redhat.com>
2019-04-23 18:04:57 -0400

    Add active stage information to the requests expendable content

--- src/smart-components/request/request-table-helpers.js
+++ src/smart-components/request/request-table-helpers.js
@@ -7,7 +7,7 @@ import { timeAgo }  from '../../helpers/shared/helpers';
 
 export const createInitialRows = data => {
   console.log('CreateInitialRows data: : ', data);
-  return data.reduce((acc, { id, requester, created_at, updated_at, active_stage, total_stages, decision, content }, key) => ([
+  return data.reduce((acc, { id, requester, created_at, updated_at, active_stage, total_stages, state, decision, content }, key) => ([
     ...acc, {
       id,
       isOpen: false,
@@ -16,7 +16,7 @@ export const createInitialRows = data => {
       timeAgo(created_at), timeAgo(updated_at), `${active_stage} of ${total_stages}`, decision ]
     }, {
       parent: key * 2,
-      cells: [{ title: <ExpandableContent id={ id } content={ content }/> }]
+      cells: [{ title: <ExpandableContent id={ id } content={ content } state={ state } active_stage={ active_stage }/> }]
     }
   ]), []);
 };
