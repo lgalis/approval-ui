83e41a14da14707a7c162cb7e9b6183a25b58ae3
Laura Galis <lgalis@redhat.com>
2019-04-26 06:56:28 -0400

    Update layout and icons for the action transcript list

--- src/smart-components/request/request-detail/action.js
+++ src/smart-components/request/request-detail/action.js
@@ -2,26 +2,34 @@ import React from 'react';
 import { timeAgo }  from '../../../helpers/shared/helpers';
 import {
   Stack,
-  StackItem,
   TextContent,
   Text,
   TextVariants
 } from '@patternfly/react-core';
+import { CheckIcon, CommentIcon, EnvelopeIcon, OutlinedTimesCircleIcon, AngleDoubleRightIcon } from '@patternfly/react-icons';
+
+const operationDisplayName = { memo: 'Comment from', approve: 'Approved By',
+  deny: 'Denied by', notify: 'Notified By', skip: 'Skipped by' };
+
+const operationIcon = { memo: <CommentIcon/>, approve: <CheckIcon/>,
+  deny: <OutlinedTimesCircleIcon/>, notify: <EnvelopeIcon/>, skip: <AngleDoubleRightIcon/> };
 
 export const ActionTrasncript = actionList => {
   return (
     <Stack>
       { Object.values(actionList)[0].map(actionItem =>
-        <Stack key={ `${actionItem.id}-action` }>
-          <StackItem style={ { textTransform: 'capitalize' } }>
-            <TextContent><Text component={ TextVariants.small }>
-              { `${timeAgo(actionItem.created_at)} by ${actionItem.processed_by}` }
-            </Text></TextContent>
-          </StackItem>
-          <StackItem style={ { textTransform: 'capitalize' } }>
+        <div key={ `${actionItem.id}-action` }>
+          <TextContent><Text style={ { marginBottom: 0 } } className="data-table-detail content" component={ TextVariants.small }>
+            { `${timeAgo(actionItem.created_at)}` }
+          </Text>
+          <Text style={ { marginBottom: 0 } } className="data-table-detail content">
+            { operationIcon[actionItem.operation] } { `${operationDisplayName[actionItem.operation]}  ${actionItem.processed_by}` }
+          </Text>
+          { actionItem.comments && <Text style={ { marginBottom: 0 } } className="data-table-detail content" component={ TextVariants.h6 }>
             { `${actionItem.comments}` }
-          </StackItem>
-        </Stack>)
+          </Text> } </TextContent>
+          <br/>
+        </div>)
       }
     </Stack>
   );
