1a5518fffc6dfa2eac7bbb7b7a8b4b10356f5f29
Laura Galis <lgalis@redhat.com>
2019-06-10 11:34:21 -0400

    Update test and snapshot for action-modal

--- src/test/smart-components/request/action-modal.test.js
+++ src/test/smart-components/request/action-modal.test.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import thunk from 'redux-thunk';
-import { shallow, mount } from 'enzyme';
 import { Provider } from 'react-redux';
+import { shallow, mount } from 'enzyme';
 import configureStore from 'redux-mock-store' ;
-import { Modal } from '@patternfly/react-core';
 import { shallowToJson } from 'enzyme-to-json';
+import { Button } from '@patternfly/react-core';
 import { MemoryRouter, Route } from 'react-router-dom';
 import promiseMiddleware from 'redux-promise-middleware';
-import { componentTypes } from '@data-driven-forms/react-form-renderer';
 import { notificationsMiddleware } from '@redhat-cloud-services/frontend-components-notifications/';
 
 import { APPROVAL_API_BASE } from '../../../utilities/constants';
-import FormRenderer from '../../../smart-components/common/form-renderer';
 import ActionModal from '../../../smart-components/request/action-modal';
+import { requestsInitialState } from '../../../redux/reducers/request-reducer';
+import { componentTypes } from '@data-driven-forms/react-form-renderer/dist/index';
 
 describe('<ActionModal />', () => {
   let initialProps;
-  let initialState;
   const middlewares = [ thunk, promiseMiddleware(), notificationsMiddleware() ];
   let mockStore;
-  const ComponentWrapper = ({ store, children, requestId }) => (
+  let initialState;
+
+  const RequestWrapper = ({ store, children, initialEntries = []}) => (
     <Provider store={ store }>
-      <MemoryRouter initialEntries={ [ `requests/${requestId}` ] }>
+      <MemoryRouter initialEntries={ initialEntries }>
         { children }
       </MemoryRouter>
     </Provider>
@@ -29,96 +30,54 @@ describe('<ActionModal />', () => {
 
   beforeEach(() => {
     initialProps = {
-
-    };
-    initialState = {
-      approvalReducer: {
-        workflows: [{
-          label: 'foo',
-          value: 'bar'
-        }]
-      },
-      requestReducer: {
-        requests: [{
-          id: '123',
-          name: 'Request'
-        }]
+      closeUrl: '/close',
+      serviceData: {
+        name: 'Foo',
+        id: '1'
       }
     };
     mockStore = configureStore(middlewares);
+    initialState = {
+      requestReducer: {
+        ...requestsInitialState,
+        isLoading: true,
+        requestData: {
+          name: 'Foo',
+          id: '1'
+        }
+      }
+    };
   });
 
   it('should render correctly', () => {
-    const store = mockStore({});
-    apiClientMock.get(`${APPROVAL_API_BASE}/workflows`, mockOnce({ body: { data: []}}));
-    const wrapper = shallow(<ComponentWrapper store={ store }><ActionModal { ...initialProps } /></ComponentWrapper>).dive();
-
-    setImmediate(() => {
-      expect(shallowToJson(wrapper)).toMatchSnapshot();
-    });
+    const wrapper = shallow(<ActionModal { ...initialProps } />);
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
   });
 
-  it('should create the AddComment variant of the action modal', done => {
+  it('should redirect back to close URL', (done) => {
     const store = mockStore(initialState);
 
-    apiClientMock.get(`${APPROVAL_API_BASE}/requests`, mockOnce({
-      body: {
-        data: [{
-          id: '123'
-        }]
+    apiClientMock.get(`${APPROVAL_API_BASE}/requests/action-modal/1`, mockOnce(
+      { body: {
+        component: componentTypes.TEXTAREA_FIELD,
+        name: 'comments',
+        type: 'text',
+        isRequired: false,
+        label: 'Comment'
       }
-    }));
-
-    const expectedSchema = {
-      fields: [{
-        component: componentTypes.TEXT_FIELD,
-        isRequired: true,
-        label: 'Comment',
-        name: 'name',
-        validate: [ expect.any(Function) ]
-      }]
-    };
+      }));
 
     const wrapper = mount(
-      <ComponentWrapper store={ store } requestId="123">
-        <Route path="requests/add_comment/:id?" render={ () => <ActionModal { ...initialProps } match={ { params: { id: '123' }} } /> }/>
-      </ComponentWrapper>
+      <RequestWrapper store={ store } initialEntries={ [ '/foo/url' ] }>
+        <Route to="/foo/url" render={ args => <ActionModal { ...initialProps } { ...args } /> }  />
+      </RequestWrapper>
     );
 
     setImmediate(() => {
-      const modal = wrapper.find(Modal);
-      const form = wrapper.find(FormRenderer);
-      expect(modal.props().title).toEqual('Add Comment');
-      expect(form.props().schema).toEqual(expectedSchema);
+      wrapper.find(Button).first().simulate('click');
+      expect(wrapper.find(MemoryRouter).instance().history.location.pathname).toEqual('/close');
       done();
     });
   });
-
-  it('should create Add comment variant of the action modal and call updateRequest on submit', () => {
-    const store = mockStore(initialState);
-
-    apiClientMock.patch(`${APPROVAL_API_BASE}/requests/add_comment/123`, ((req, res) => {
-      expect(JSON.parse(req.body())).toEqual({ id: '123', name: 'Request', comment: 'Test' });
-      return res.body(200);
-    }));
-
-    apiClientMock.get(`${APPROVAL_API_BASE}/requests`, mockOnce({ body: {
-      data: [{
-        label: 'foo',
-        value: 'bar'
-      }]
-    }}));
-
-    const wrapper = mount(
-      <ComponentWrapper store={ store } requestId="123">
-        <Route
-          path="requests/add_comment/:id?"
-          render={ () => <ActionModal { ...initialProps } match={ { params: { id: '123' }} }/> }
-        />
-      </ComponentWrapper>
-    );
-
-    const button = wrapper.find('button').last();
-    button.simulate('click');
-  });
 });
+
