b03eeab91752ca34fd78a4489650b478b997e3f7
Laura Galis <lgalis@redhat.com>
2019-08-07 18:13:25 -0400

    Update stage information modal layout

--- src/smart-components/workflow/edit-workflow-modal.js
+++ src/smart-components/workflow/edit-workflow-modal.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { connect, useSelector } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router-dom';
-import { ActionGroup, Button, FormGroup, Modal, Title } from '@patternfly/react-core';
+import { ActionGroup, Button, FormGroup, Modal, Split, SplitItem, Stack, StackItem, Title } from '@patternfly/react-core';
 import { addNotification } from '@redhat-cloud-services/frontend-components-notifications';
 import { addWorkflow, updateWorkflow, fetchWorkflow } from '../../redux/actions/workflow-actions';
 import { WorkflowStageLoader } from '../../presentational-components/shared/loader-placeholders';
@@ -81,30 +81,44 @@ const EditWorkflowModal = ({
       isOpen
       onClose={ onCancel }
       onSave={ onSave }>
-      <FormGroup>
-        { isFetching && <WorkflowStageLoader/> }
-        { !isFetching && rbacGroups.length === 0 && (
-          <Title headingLevel="h2" size="1xl">
-                No groups available.
-          </Title>) }
-        { !isFetching && rbacGroups.length > 0 && editType === 'stages' && (
-          <SetStages formData={ formData }
-            handleChange = { handleChange }
-            options={ groupOptions }
-            title={ `Add or remove ${formData.name}'s stages` }/>) }
-        { !isFetching && editType !== 'stages' && (
-          <StageInformation formData = { formData } handleChange = { handleChange } title = { `Make any changes to workflow ${formData.name}` }/>) }
-      </FormGroup>
-      <ActionGroup>
-        <Button aria-label={ 'Save' }
-          variant="primary"
-          type="submit"
-          onClick={ onSave }>Save</Button>
-        <Button  aria-label='Cancel'
-          variant='secondary'
-          type='button'
-          onClick={ onCancel }>Cancel</Button>
-      </ActionGroup>
+      <Stack gutter="md">
+        <StackItem>
+          <FormGroup>
+            { isFetching && <WorkflowStageLoader/> }
+            { !isFetching && rbacGroups.length === 0 && (
+              <Title headingLevel="h2" size="1xl">
+                    No groups available.
+              </Title>) }
+            { !isFetching && rbacGroups.length > 0 && editType === 'stages' && (
+              <SetStages formData={ formData }
+                handleChange = { handleChange }
+                options={ groupOptions }
+                title={ `Add or remove ${formData.name}'s stages` }/>) }
+            { !isFetching && editType !== 'stages' && (
+              <StageInformation formData = { formData }
+                handleChange = { handleChange }
+                title = { `Make any changes to workflow ${formData.name}` }/>) }
+          </FormGroup>
+        </StackItem>
+        <StackItem>
+          <ActionGroup>
+            <Split gutter="md">
+              <SplitItem>
+                <Button aria-label={ 'Save' }
+                  variant="primary"
+                  type="submit"
+                  onClick={ onSave }>Save</Button>
+              </SplitItem>
+              <SplitItem>
+                <Button  aria-label='Cancel'
+                  variant='secondary'
+                  type='button'
+                  onClick={ onCancel }>Cancel</Button>
+              </SplitItem>
+            </Split>
+          </ActionGroup>
+        </StackItem>
+      </Stack>
     </Modal>
   );
 };
