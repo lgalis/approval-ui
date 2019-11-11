2461756516abaa5cdd1938155d58ee5ee0de35b9
Martin Marosi <marvusm.mmi@gmail.com>
2019-11-11 09:57:07 +0100

    Added tests for stage component

--- src/smart-components/request/request-detail/stage.js
+++ src/smart-components/request/request-detail/stage.js
@@ -44,11 +44,15 @@ class Stage extends Component {
       <Dropdown
         position={ DropdownPosition.right }
         onSelect={ this.onKebabSelect }
-        toggle={ <KebabToggle onToggle={ this.onKebabToggle }/> }
+        toggle={ <KebabToggle id={ `stage-request-dropdown-${stage.request_id}` } onToggle={ this.onKebabToggle }/> }
         isOpen = { this.state.isKebabOpen }
         dropdownItems={ [
           <DropdownItem aria-label="Add Comment" key={ `add_comment_${stage.id}` }>
-            <Link to={ `/requests/detail/${stage.request_id}/add_comment` } className="pf-c-dropdown__menu-item">
+            <Link
+              id={ `stage-${stage.request_id}-request-comment` }
+              to={ `/requests/detail/${stage.request_id}/add_comment` }
+              className="pf-c-dropdown__menu-item"
+            >
               Comment
             </Link>
           </DropdownItem>
@@ -90,12 +94,12 @@ class Stage extends Component {
                   <LevelItem>
                     { (requestActive && this.props.isActive) &&
                     <div>
-                      <Link to={ `/requests/detail/${item.request_id}/approve` }>
+                      <Link id={ `approve-${item.request_id}` } to={ `/requests/detail/${item.request_id}/approve` }>
                         <Button variant="link" aria-label="Approve Request">
                           Approve
                         </Button>
                       </Link>
-                      <Link to={ `/requests/detail/${item.request_id}/deny` }>
+                      <Link id={ `deny-${item.request_id}` } to={ `/requests/detail/${item.request_id}/deny` }>
                         <Button variant="link" className="destructive-color" aria-label="Deny Request">
                           Deny
                         </Button>
@@ -133,7 +137,13 @@ class Stage extends Component {
 Stage.propTypes = {
   isLoading: PropTypes.bool,
   isActive: PropTypes.bool,
-  item: PropTypes.object,
+  item: PropTypes.shape({
+    state: PropTypes.string,
+    request_id: PropTypes.string,
+    stageActions: PropTypes.shape({
+      data: PropTypes.array
+    })
+  }).isRequired,
   idx: PropTypes.number,
   isExpanded: PropTypes.func.isRequired,
   toggleExpand: PropTypes.func.isRequired,
---
+++ src/test/smart-components/request/request-detail/stage.test.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import { MemoryRouter } from 'react-router-dom';
+import Stage from '../../../../smart-components/request/request-detail/stage';
+
+const ComponentWrapper = ({ children }) => (
+  <MemoryRouter initialEntries={ [ '/foo' ] }>
+    { children }
+  </MemoryRouter>
+);
+
+describe('<Stage />', () => {
+  let initialProps;
+  beforeEach(() => {
+    initialProps = {
+      isExpanded: jest.fn(),
+      toggleExpand: jest.fn(),
+      item: {
+        id: 'item-id',
+        state: 'no-state',
+        request_id: 'request-id',
+        stageActions: {
+          data: []
+        }
+      }
+    };
+  });
+
+  it('should render correctly', () => {
+    const wrapper = mount(<ComponentWrapper><Stage { ...initialProps } /></ComponentWrapper>).find(Stage);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('should render item in active state with link', () => {
+    const wrapper = mount(
+      <ComponentWrapper>
+        <Stage
+          { ...initialProps }
+          item={ {
+            id: 'item-id',
+            state: 'notified',
+            request_id: 'request-id',
+            stageActions: {
+              data: []
+            }
+          } }
+          isActive
+        />
+      </ComponentWrapper>
+    );
+    wrapper.update();
+    expect(wrapper.find('a#approve-request-id')).toHaveLength(1);
+    expect(wrapper.find('a#deny-request-id')).toHaveLength(1);
+
+    wrapper.find('Link#approve-request-id').simulate('click', { button: 0 });
+    wrapper.update();
+    const history = wrapper.find(MemoryRouter).instance().history;
+    expect(history.location.pathname).toEqual('/requests/detail/request-id/approve');
+    wrapper.find('Link#deny-request-id').simulate('click', { button: 0 });
+    expect(history.location.pathname).toEqual('/requests/detail/request-id/deny');
+  });
+
+  it('should expand item', () => {
+    const toggleExpand = jest.fn();
+    const wrapper = mount(
+      <ComponentWrapper>
+        <Stage
+          { ...initialProps }
+          item={ {
+            id: 'item-id',
+            state: 'notified',
+            request_id: 'request-id',
+            stageActions: {
+              data: []
+            }
+          } }
+          isActive
+          toggleExpand={ toggleExpand }
+        />
+      </ComponentWrapper>
+    );
+    wrapper.update();
+    wrapper.find('button.pf-c-button.pf-m-plain').simulate('click');
+    wrapper.update();
+    expect(toggleExpand).toHaveBeenCalledWith('stage-item-id');
+  });
+
+  it('should expand kebab menu', () => {
+    const toggleExpand = jest.fn();
+    const wrapper = mount(
+      <ComponentWrapper>
+        <Stage
+          { ...initialProps }
+          item={ {
+            id: 'item-id',
+            state: 'notified',
+            request_id: 'request-id',
+            stageActions: {
+              data: []
+            }
+          } }
+          isActive
+          toggleExpand={ toggleExpand }
+        />
+      </ComponentWrapper>
+    );
+    wrapper.update();
+    wrapper.find('#stage-request-dropdown-request-id').first().simulate('click');
+    wrapper.update();
+    wrapper.find('Link#stage-request-id-request-comment').first().simulate('click', { button: 0 });
+    wrapper.update();
+  });
+});
