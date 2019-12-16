df1e47832c8537fbf9435220ae9f0ed219185ece
Laura Galis <lgalis@redhat.com>
2019-12-16 17:50:19 -0500

    Added placeholder while the group names are loading in the expanded data

--- src/smart-components/workflow/expandable-content.js
+++ src/smart-components/workflow/expandable-content.js
@@ -2,9 +2,11 @@ import React, { Fragment, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { TextContent, Text, TextVariants } from '@patternfly/react-core';
 import { fetchGroupName } from '../../helpers/group/group-helper';
+import { Skeleton, SkeletonSize } from '@redhat-cloud-services/frontend-components';
 
 const ExpandableContent = ({ description, groupRefs }) => {
   const [ groupNames, setGroupNames ] = useState([]);
+  const [ isLoaded, setIsLoaded ] = useState();
 
   const fetchGroupNames = async() => {
     const names = [];
@@ -15,7 +17,7 @@ const ExpandableContent = ({ description, groupRefs }) => {
   };
 
   useEffect(() => {
-    fetchGroupNames(groupRefs).then((data) => setGroupNames(data));
+    fetchGroupNames(groupRefs).then((data) => { setGroupNames(data); setIsLoaded(true); }).catch(() => setIsLoaded(true));
   }, []);
 
   return (
@@ -27,10 +29,14 @@ const ExpandableContent = ({ description, groupRefs }) => {
       <TextContent>
         <Fragment>
           <Text className="data-table-detail heading" component={ TextVariants.small }>Groups</Text>
-          <Text className="data-table-detail content"
-            component={ TextVariants.h5 }>
-            { groupNames.join(',') }
-          </Text>
+          { isLoaded ?
+            <Text className="data-table-detail content"
+              component={ TextVariants.h5 }>
+              { groupNames.join(',') }
+            </Text> :
+            <div>
+              <Skeleton size={ SkeletonSize.sm } />
+            </div> }
         </Fragment>
       </TextContent>
     </Fragment>
