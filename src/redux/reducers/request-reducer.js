dedc278b83bc3b8f0caac01555505b4c40536629
Laura Galis <lgalis@redhat.com>
2019-12-19 17:09:32 -0500

    Redux methods to get the request content. Refactor the request_detail component to use hooks. Add request content to the info bar

--- src/redux/reducers/request-reducer.js
+++ src/redux/reducers/request-reducer.js
@@ -1,5 +1,6 @@
 import {
   FETCH_REQUEST,
+  FETCH_REQUEST_CONTENT,
   FETCH_REQUESTS
 } from '../../redux/action-types';
 
@@ -7,6 +8,7 @@ import {
 export const requestsInitialState = {
   requests: [],
   selectedRequest: {},
+  requestContent: {},
   filterValue: '',
   isRequestDataLoading: false
 };
@@ -14,10 +16,13 @@ export const requestsInitialState = {
 const setLoadingState = state => ({ ...state, isRequestDataLoading: true });
 const setRequests = (state, { payload }) => ({ ...state, requests: payload, isRequestDataLoading: false });
 const selectRequest = (state, { payload }) => ({ ...state, selectedRequest: payload, isRequestDataLoading: false });
+const setRequestContent = (state, { payload }) => ({ ...state, requestContent: payload, isRequestDataLoading: false });
 
 export default {
   [`${FETCH_REQUESTS}_PENDING`]: setLoadingState,
   [`${FETCH_REQUESTS}_FULFILLED`]: setRequests,
   [`${FETCH_REQUEST}_PENDING`]: setLoadingState,
-  [`${FETCH_REQUEST}_FULFILLED`]: selectRequest
+  [`${FETCH_REQUEST}_FULFILLED`]: selectRequest,
+  [`${FETCH_REQUEST_CONTENT}_PENDING`]: setLoadingState,
+  [`${FETCH_REQUEST_CONTENT}_FULFILLED`]: setRequestContent
 };
--- src/smart-components/request/request-detail/request-detail.js
+++ src/smart-components/request/request-detail/request-detail.js
@@ -1,35 +1,51 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { withRouter, Route } from 'react-router-dom';
+import { Route, useRouteMatch } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import { Grid, GridItem } from '@patternfly/react-core';
 import { Section } from '@redhat-cloud-services/frontend-components';
 import '../../../App.scss';
 import ActionModal from '../action-modal';
 import RequestInfoBar from './request-info-bar';
 import RequestTranscript from './request-transcript';
-import { fetchRequest } from '../../../redux/actions/request-actions';
+import { fetchRequest, fetchRequestContent } from '../../../redux/actions/request-actions';
 import { RequestLoader } from '../../../presentational-components/shared/loader-placeholders';
 import { TopToolbar, TopToolbarTitle } from '../../../presentational-components/shared/top-toolbar';
 
-const RequestDetail = ({
-  match: { params: { id }, url },
-  isLoading,
-  fetchRequest
-}) => {
-  const [ selectedRequest, setSelectedRequest ] = useState({});
+const initialState = {
+  isFetching: true
+};
+const requestState = (state, action) => {
+  switch (action.type) {
+    case 'setFetching':
+      return { ...state, isFetching: action.payload };
+    default:
+      return state;
+  }
+};
 
-  const fetchData = () => {
-    fetchRequest(id).then((data) => setSelectedRequest(data.value));
-  };
+const RequestDetail = () => {
+  const [{ isFetching }, stateDispatch ] = useReducer(requestState, initialState);
+
+  const { selectedRequest, requestContent } = useSelector(
+    ({
+      requestReducer: {
+        requestContent: requestContent,
+        selectedRequest: selectedRequest
+      }
+    }) => ({ selectedRequest, requestContent })
+  );
+  const match = useRouteMatch('/requests/detail/:id');
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    fetchData();
+    dispatch(fetchRequest(match.id));
+    dispatch(fetchRequestContent(match.id))
+    .then(() => stateDispatch({ type: 'setFetching', payload: false }));
   }, []);
 
   const renderRequestDetails = () => {
-    if (isLoading || !selectedRequest || Object.keys(selectedRequest).length === 0) {
+    if (isFetching || !selectedRequest || Object.keys(selectedRequest).length === 0) {
       return (
         <Section style={ { backgroundColor: 'white', minHeight: '100%' } }>
           <RequestLoader />
@@ -40,10 +56,10 @@ const RequestDetail = ({
       return (
         <Fragment>
           <GridItem md={ 2 } className="detail-pane">
-            <RequestInfoBar request={ selectedRequest }/>
+            <RequestInfoBar request={ selectedRequest } requestContent={ requestContent }/>
           </GridItem>
           <GridItem md={ 10 } className="detail-pane">
-            <RequestTranscript request={ selectedRequest } url={ url }/>
+            <RequestTranscript request={ selectedRequest } url={ match.url }/>
           </GridItem>
         </Fragment>
       );
@@ -53,16 +69,16 @@ const RequestDetail = ({
   return (
     <Fragment>
       <Route exact path="/requests/detail/:id/add_comment" render={ props =>
-        <ActionModal { ...props } actionType={ 'Add Comment' } closeUrl={ url } postMethod={ fetchData }/> }/>
+        <ActionModal { ...props } actionType={ 'Add Comment' } closeUrl={ match.url } postMethod={ fetchData }/> }/>
       <Route exact path="/requests/detail/:id/approve" render={ props =>
-        <ActionModal { ...props } actionType={ 'Approve' } closeUrl={ url } postMethod={ fetchData } /> } />
+        <ActionModal { ...props } actionType={ 'Approve' } closeUrl={ match.url } postMethod={ fetchData } /> } />
       <Route exact path="/requests/detail/:id/deny" render={ props =>
-        <ActionModal { ...props } actionType={ 'Deny' } closeUrl={ url } postMethod={ fetchData }/> } />
+        <ActionModal { ...props } actionType={ 'Deny' } closeUrl={ match.url } postMethod={ fetchData }/> } />
       <TopToolbar
         breadcrumbs={ [{ title: 'Request Queue', to: '/requests', id: 'requests' }] }
         paddingBottom={ true }
       >
-        <TopToolbarTitle title={ `Request ${id}` } />
+        <TopToolbarTitle title={ `Request ${match.id}` } />
       </TopToolbar>
       <Section type="content">
         <Grid gutter="md">
@@ -74,28 +90,7 @@ const RequestDetail = ({
 };
 
 RequestDetail.propTypes = {
-  match: PropTypes.shape({
-    url: PropTypes.string.isRequired,
-    params: PropTypes.shape({
-      id: PropTypes.string
-    })
-  }).isRequired,
-  history: PropTypes.shape({
-    push: PropTypes.func.isRequired
-  }).isRequired,
-  isLoading: PropTypes.bool,
-  id: PropTypes.string,
-  fetchRequest: PropTypes.func.isRequired
+  isLoading: PropTypes.bool
 };
 
-const mapStateToProps = (state) => {
-  return {
-    isLoading: state.requestReducer.isRequestDataLoading
-  };
-};
-
-const mapDispatchToProps = dispatch => bindActionCreators({
-  fetchRequest
-}, dispatch);
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(RequestDetail));
+export default RequestDetail;
