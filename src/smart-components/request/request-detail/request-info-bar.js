23d0eac77152cae8aafcb84fb56e0e21cd4b5048
Laura Galis <lgalis@redhat.com>
2020-01-15 17:36:41 -0500

    Updated layout for the request detail info bar

--- src/smart-components/request/request-detail/request-info-bar.js
+++ src/smart-components/request/request-detail/request-info-bar.js
@@ -3,25 +3,18 @@ import PropTypes from 'prop-types';
 import { Text, TextContent, TextVariants } from '@patternfly/react-core';
 
 const RequestInfoBar = ({ request, requestContent }) => {
+  console.log('DEBUG info bar: request, requestContent: ', request, requestContent);
   return (
     <TextContent>
       <Text component={ TextVariants.small }>Product: <br/></Text>
-      <Text>{ requestContent ? requestContent.product : 'Unknown' }</Text>
-      <Text component={ TextVariants.small }>
-              Portfolio:
-        <br/>
-      </Text>
-      <Text>
-        { requestContent ? requestContent.portfolio : '' }
-      </Text>
+      <Text>{ requestContent ? requestContent.product : '' }</Text>
+      <Text component={ TextVariants.small }>Portfolio: <br/></Text>
+      <Text>{ requestContent ? requestContent.portfolio : '' }</Text>
       <Text component={ TextVariants.small }>Platform: <br/></Text>
-      <Text>{ requestContent ? requestContent.platform : '' }</Text>
-      <Text component={ TextVariants.small }>Project: <br/></Text>
-      <Text>{ requestContent ? requestContent.project : '' }</Text>
-      <Text component={ TextVariants.small }>Order Parameters: </Text>
+      <Text>{ requestContent ? requestContent.platform : ' ' }</Text>
       <Text component={ TextVariants.small }>Requester: </Text>
       <Text component={ TextVariants.h6 }>{ request.requester_name }</Text>
-      <Text component={ TextVariants.h6 }>Project: <br/> </Text>
+      <Text component={ TextVariants.h6 }>Order Parameters: <br/> </Text>
       { requestContent.params && Object.keys(requestContent.params).map(param => {
         return ((requestContent.params[param]) &&
                   <Text key={ param } component={ TextVariants.small }>
