1efe4d2f8528a0bae857d1d084ba7493c709b5da
Laura Galis <lgalis@redhat.com>
2020-02-06 14:59:32 -0500

    Use group_name in the request detail for sub-requests

--- src/smart-components/request/request-detail/request.js
+++ src/smart-components/request/request-detail/request.js
@@ -84,7 +84,7 @@ class Request extends Component {
           <DataListItemCells
             dataListCells={ [
               <DataListCell key={ item.id }>
-                <span id={ item.id }>{ `${this.props.idx + 1}. ${item.name}` } </span>
+                <span id={ `${item.id}-name` }>{ `${this.props.idx + 1}. ${item.parent_id ? item.group_name : item.name}` } </span>
               </DataListCell>,
               <DataListCell key={ `${item.id}-state` }>
                 <span style={ { textTransform: 'capitalize' } } id={ `${item.id}-state` }>{ `${item.state}` } </span>
@@ -140,6 +140,8 @@ Request.propTypes = {
     id: PropTypes.string,
     name: PropTypes.string,
     state: PropTypes.string,
+    parent_id: PropTypes.string,
+    group_name: PropTypes.string,
     requestActions: PropTypes.shape({
       data: PropTypes.array
     })
