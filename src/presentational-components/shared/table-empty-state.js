14cbf2544fbe01b7bdff3cfe4a6f34c3cdbf8e06
Laura Galis <lgalis@redhat.com>
2020-02-12 15:30:00 -0500

    Added empty state page for the approval processes list

---
+++ src/presentational-components/shared/table-empty-state.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Button,
+  EmptyState,
+  EmptyStateIcon,
+  EmptyStateBody,
+  EmptyStateSecondaryActions,
+  Text,
+  TextContent,
+  TextVariants
+} from '@patternfly/react-core';
+import { Link }  from 'react-router-dom';
+
+const TableEmptyState = ({
+  title,
+  Icon,
+  description,
+  PrimaryAction,
+  renderDescription
+}) => (
+  <div className="pf-u-mt-xl">
+    <EmptyState className="pf-u-ml-auto pf-u-mr-auto">
+      <EmptyStateIcon icon={ Icon } />
+      <TextContent>
+        <Text component={ TextVariants.h1 }>{ title }</Text>
+      </TextContent>
+      <EmptyStateBody>
+        { description }
+        { renderDescription() }
+      </EmptyStateBody>
+      <EmptyStateSecondaryActions>
+        { PrimaryAction && <PrimaryAction /> }
+      </EmptyStateSecondaryActions>
+    </EmptyState>
+  </div>
+);
+
+TableEmptyState.defaultProps = {
+  renderDescription: () => null
+};
+
+TableEmptyState.propTypes = {
+  title: PropTypes.string.isRequired,
+  Icon: PropTypes.any.isRequired,
+  description: PropTypes.string.isRequired,
+  PrimaryAction: PropTypes.any,
+  renderDescription: PropTypes.func
+};
+
+export default TableEmptyState;
+
+export const EmptyStatePrimaryAction = ({ url, label }) => (
+  <Link to={ url }>
+    <Button variant="secondary">{ label }</Button>
+  </Link>
+);
+
+EmptyStatePrimaryAction.propTypes = {
+  url: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired
+};
