25e993d9542d8ca996d656ecaa71f9c3df68cb10
Laura Galis <lgalis@redhat.com>
2020-03-23 17:23:07 -0400

    Add error page to routes

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import React, { lazy, Suspense } from 'react';
 import { RequestLoader } from './presentational-components/shared/loader-placeholders';
 import ProtectedRoute from './routing/protected-route';
+import CommonApiError from './smart-components/error-pages/common-api-error';
 
 const Requests = lazy(() => import(/* webpackChunkName: "requests" */ './smart-components/request/requests'));
 const Workflows = lazy(() => import(/* webpackChunkName: "workflows" */ './smart-components/workflow/workflows'));
@@ -10,11 +11,14 @@ const paths = {
   requests: '/requests',
   workflows: '/workflows'
 };
+const errorPaths = [ '/400', '/401', '/404' ];
+
 export const Routes = () => {
   return <Suspense fallback={ <RequestLoader /> }>
     <Switch>
       <ProtectedRoute path={ paths.workflows } component={ Workflows }/>
       <Route path={ paths.requests } component={ Requests }/>
+      <Route path={ errorPaths } component={ CommonApiError } />
       <Route>
         <Redirect to={ paths.requests } />
       </Route>
---
+++ src/routing/protected-route.js
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+import { Route, Redirect, useLocation } from 'react-router-dom';
+import UserContext from '../user-context';
+import { isApprovalAdmin } from '../helpers/shared/helpers';
+
+const ProtectedRoute = (props) => {
+  const { roles: userRoles } = useContext(UserContext);
+  const location = useLocation();
+
+  return  isApprovalAdmin(userRoles) ? (
+    <Route { ...props } />
+  ) : (
+    <Redirect
+      to={ {
+        pathname: '/401',
+        state: {
+          from: location
+        }
+      } }
+    />
+  );
+};
+
+export default ProtectedRoute;
