9e06773f6db2751053072617be36fc1bc2c3bf03
Laura Galis <lgalis@redhat.com>
2020-03-27 16:20:37 -0400

    Split request info bar into 2 cards

--- src/smart-components/request/request-detail/request-info-bar.js
+++ src/smart-components/request/request-detail/request-info-bar.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Text, TextContent, TextVariants } from '@patternfly/react-core';
+import { Card, Fragment, Text, TextContent, TextVariants } from '@patternfly/react-core';
 
 const RequestInfoBar = ({ request, requestContent }) => {
   return (
-    <TextContent>
-      <Text component={ TextVariants.small }>Product: <br/></Text>
-      <Text>{ requestContent ? requestContent.product : '' }</Text>
-      <Text component={ TextVariants.small }>Portfolio: <br/></Text>
-      <Text>{ requestContent ? requestContent.portfolio : '' }</Text>
-      <Text component={ TextVariants.small }>Platform: <br/></Text>
-      <Text>{ requestContent ? requestContent.platform : ' ' }</Text>
-      <Text component={ TextVariants.small }>Requester: </Text>
-      <Text component={ TextVariants.h6 }>{ request.requester_name }</Text>
-      <Text component={ TextVariants.h6 }>Order Parameters: <br/> </Text>
-      { requestContent.params && Object.keys(requestContent.params).map(param => {
-        return ((requestContent.params[param]) &&
-                  <Text key={ param } component={ TextVariants.small }>
-                    { `${param}: ${requestContent.params[param]}` }
-                  </Text>);
-      })
-      }
-    </TextContent>
+    <Fragment>
+      <Card>
+        <TextContent>
+          <Text component={ TextVariants.small }>Product: <br/></Text>
+          <Text>{ requestContent ? requestContent.product : '' }</Text>
+          <Text component={ TextVariants.small }>Portfolio: <br/></Text>
+          <Text>{ requestContent ? requestContent.portfolio : '' }</Text>
+          <Text component={ TextVariants.small }>Platform: <br/></Text>
+          <Text>{ requestContent ? requestContent.platform : ' ' }</Text>
+          <Text component={ TextVariants.small }>Requester: </Text>
+          <Text component={ TextVariants.h6 }>{ request.requester_name }</Text>
+        </TextContent>
+      </Card>
+      <Card>
+        <TextContent>
+          <Text component={ TextVariants.h6 }>Order Parameters: <br/> </Text>
+          { requestContent.params && Object.keys(requestContent.params).map(param => {
+            return ((requestContent.params[param]) &&
+                <Text key={ param } component={ TextVariants.small }>
+                  { `${param}: ${requestContent.params[param]}` }
+                </Text>);
+          })
+          }
+        </TextContent>
+      </Card>
+    </Fragment>
   );
 };
 
