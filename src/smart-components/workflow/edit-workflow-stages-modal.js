c10ae472d5ce6437f26f53222500c692bbf3bda1
Laura Galis <lgalis@redhat.com>
2020-04-03 12:01:33 -0400

    Update tests to include group name

--- src/smart-components/workflow/edit-workflow-stages-modal.js
+++ src/smart-components/workflow/edit-workflow-stages-modal.js
@@ -41,7 +41,7 @@ const EditWorkflowStagesModal = ({
   const onSave = () => {
     const { wfGroups } = formData;
     const workflowData = { group_refs: wfGroups ? wfGroups.map(group => { const group_ref = group ? { name: group.label, uuid: group.value } : {};
-        return group_ref; }) : []};
+      return group_ref; }) : []};
     updateWorkflow({ id, ...workflowData }).then(() => postMethod()).then(()=>push('/workflows'));
   };
 
