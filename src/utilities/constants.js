349bfa369adb927ad8fe88bc7fef285a27118a86
Laura Galis <lgalis@redhat.com>
2020-04-07 07:38:02 -0400

    Update closeUrl for action modal

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,3 +1,5 @@
 export const APPROVAL_API_BASE = `${process.env.BASE_PATH}/approval/v1.2`;
 export const RBAC_API_BASE = `${process.env.BASE_PATH}/rbac/v1`;
 
+export const REQUEST_DETAIL_ROUTE = `/requests/detail/`;
+
