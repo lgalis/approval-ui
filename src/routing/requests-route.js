6dff105e771ae93b6b2a6e1af7498eb0faf87990
Laura Galis <lgalis@redhat.com>
2020-06-01 13:54:47 -0400

    Remove allrequest redirection for no approval role

--- src/routing/requests-route.js
+++ src/routing/requests-route.js
@@ -1,26 +1,15 @@
 import React, { useContext } from 'react';
 import { Route, Redirect, useLocation } from 'react-router-dom';
 import UserContext from '../user-context';
-import paths from './../constants/routes';
 import { isApprovalAdmin, isApprovalApprover } from '../helpers/shared/helpers';
 
 const RequestsRoute = (props) => {
   const { userRoles: userRoles } = useContext(UserContext);
   const location = useLocation();
 
-  if (isApprovalApprover(userRoles)) {
+  if (isApprovalApprover(userRoles) || isApprovalAdmin(userRoles)) {
     return <Route { ...props } />;
-  } else if (isApprovalAdmin(userRoles)) {
-    return <Redirect
-      to={ {
-        pathname: paths.allrequests,
-        state: {
-          from: location
-        }
-      } }
-    />;
-  }
-  else {
+  } else {
     return <Redirect
       to={ {
         pathname: '/403',
