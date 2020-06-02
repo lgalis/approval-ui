b2c3f91b0b2caef2a8753c3dd08497b5bc52d0ea
Laura Galis <lgalis@redhat.com>
2020-06-02 16:54:36 -0400

    Clear request list when switching between tabs ( personas )

--- src/smart-components/app-tabs/app-tabs.js
+++ src/smart-components/app-tabs/app-tabs.js
@@ -1,23 +1,15 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { useLocation, useHistory } from 'react-router-dom';
 import { Tabs, Tab } from '@patternfly/react-core';
-import UserContext from '../../user-context';
-import { isApprovalApprover } from '../../helpers/shared/helpers';
 
-export const approvalTabItems = [{ eventKey: 0, title: 'Request queue', name: '/requests' },
+const approvalTabItems = [{ eventKey: 0, title: 'Request queue', name: '/requests' },
   { eventKey: 1, title: 'All requests', name: '/allrequests' },
   { eventKey: 2, title: 'Approval processes', name: '/workflows' }];
 
-export const adminTabItems = [{ eventKey: 0, title: 'All requests', name: '/allrequests' },
-  { eventKey: 1, title: 'Approval processes', name: '/workflows' }];
-
-export const AppTabs = () => {
+export const AppTabs = ({ tabItems = approvalTabItems }) => {
   const history = useHistory();
   const { pathname, search } = useLocation();
-  const { userRoles: userRoles } = useContext(UserContext);
-  const tabItems = isApprovalApprover(userRoles) ? approvalTabItems : adminTabItems;
-
   const activeTab = tabItems.find(({ name }) => pathname.includes(name));
   const handleTabClick = (_event, tabIndex) =>
     history.push({ pathname: tabItems[tabIndex].name, search });
