5f40c1ba9391e48c1b62457d10b32c4f749a24d0
Laura Galis <lgalis@redhat.com>
2020-06-04 16:26:19 -0400

    Update 403 error page to include Approval title

--- src/smart-components/error-pages/common-api-error.js
+++ src/smart-components/error-pages/common-api-error.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import {
   Bullseye,
@@ -10,47 +10,47 @@ import {
   EmptyStatePrimary
 } from '@patternfly/react-core';
 import LockIcon from '@patternfly/react-icons/dist/js/icons/lock-icon';
+import { TopToolbar, TopToolbarTitle } from '../../presentational-components/shared/top-toolbar';
 
 const TITLES = {
   '/401': 'Unauthorized',
-  '/403': 'Forbidden'
+  '/403': 'You do not have access to Approval'
 };
 
 const MESSAGES = {
   '/401': 'You are not authorized to access this section: ',
-  '/403': 'You do not have access to Approval'
+  '/403': 'Contact your organization administrator for more information'
 };
 
 const CommonApiError = () => {
   const { state, pathname } = useLocation();
   const { history } = useHistory();
   return (
-    <Bullseye className="global-primary-background">
-      <EmptyState>
-        <div>
-          <EmptyStateIcon icon={ LockIcon } />
-        </div>
-        <div>
-          <Title size="lg">{ TITLES[pathname] }</Title>
-        </div>
-        <EmptyStateBody>
-          { MESSAGES[pathname] }
-          <span>
-            { state?.from?.pathname }
-            { state?.from?.search }
-          </span>
-          <br />
-          Contact your organization administrator for more information.
-        </EmptyStateBody>
-        <EmptyStatePrimary>
-          {
-            document.referrer ?
-              <Button variant="primary" onClick={ () => history.back() }>Return to previous page</Button> :
-              <Button variant="primary" component="a" href=".">Go to landing page</Button>
-          }
-        </EmptyStatePrimary>
-      </EmptyState>
-    </Bullseye>
+    <Fragment>
+      <TopToolbar className="pf-u-pb-md">
+        <TopToolbarTitle title="Approval"/>
+      </TopToolbar>
+      <div className="pf-u-mb-xl">
+        <EmptyState className="pf-u-ml-auto pf-u-mr-auto">
+          <div>
+            <EmptyStateIcon icon={ LockIcon } />
+          </div>
+          <div>
+            <Title size="lg">{ TITLES[pathname] }</Title>
+          </div>
+          <EmptyStateBody>
+            { MESSAGES[pathname] }
+          </EmptyStateBody>
+          <EmptyStatePrimary>
+            {
+              document.referrer ?
+                <Button variant="primary" onClick={ () => history.back() }>Return to previous page</Button> :
+                <Button variant="primary" component="a" href=".">Go to landing page</Button>
+            }
+          </EmptyStatePrimary>
+        </EmptyState>
+      </div>
+      </Fragment>
   );
 };
 
