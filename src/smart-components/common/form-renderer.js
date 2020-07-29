4bbe80da53fcc7aa476bc0260b9850db317b5972
Richard Vsiansky <r.vsia@seznam.cz>
2020-07-29 11:43:03 +0200

    Import only used components

--- src/smart-components/common/form-renderer.js
+++ src/smart-components/common/form-renderer.js
@@ -2,12 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import ReactFormRender from '@data-driven-forms/react-form-renderer/dist/cjs/form-renderer';
-import componentMapper from '@data-driven-forms/pf4-component-mapper/dist/cjs/component-mapper';
+import componentTypes from '@data-driven-forms/react-form-renderer/dist/cjs/component-types';
+
 import FormTemplate from '@data-driven-forms/pf4-component-mapper/dist/cjs/form-template';
+import Select from '@data-driven-forms/pf4-component-mapper/dist/cjs/select';
+import Textarea from '@data-driven-forms/pf4-component-mapper/dist/cjs/textarea';
+import TextField from '@data-driven-forms/pf4-component-mapper/dist/cjs/text-field';
+import SubForm from '@data-driven-forms/pf4-component-mapper/dist/cjs/sub-form';
 
 const FormRenderer = ({ formContainer, ...rest }) => (
   <ReactFormRender
-    componentMapper={ componentMapper }
+    componentMapper={ {
+      [componentTypes.SELECT]: Select,
+      [componentTypes.TEXTAREA]: Textarea,
+      [componentTypes.TEXT_FIELD]: TextField,
+      [componentTypes.SUB_FORM]: SubForm
+    } }
     FormTemplate={ (props) => <FormTemplate { ...props }/> }
     { ...rest }
   />
